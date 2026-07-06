61b0320273c39980c2e7186c2672b6a5d2bbffbf
crivella <davidecrivella@yahoo.it>
2026-07-06 11:59:41 +0200

    Generalize kernel_data commands through a `ModuleKernelMapping` mapping and add octave kernels

--- easybuild_jupyter_kernels/kernelspec.py
+++ easybuild_jupyter_kernels/kernelspec.py
@@ -2,35 +2,115 @@
 import os
 import subprocess
 import sys
-from dataclasses import dataclass, field, fields
+from collections.abc import Mapping
+from dataclasses import asdict, dataclass, field, fields
 from functools import lru_cache
 
 from jupyter_client.kernelspec import (KernelSpec, KernelSpecManager,
                                        NoSuchKernel)
 
+DISPLAY_PREFIX = os.getenv('EB_JUPYTER_KERNEL_DISPLAY_PREFIX', 'EasyBuild\'s')
+
+
+@dataclass
+class ModuleKernelMapping(Mapping):
+    """Data class to hold information about a kernel spec derived from an EasyBuild module."""
+    kernel_display_name: str = field(metadata={
+        'help': 'Display name of the kernel (e.g., Python 3)',
+    })
+    kernel_language: str = field(metadata={
+        'help': 'Language of the kernel (e.g., python)',
+    })
+    kernel_path: str = field(metadata={
+        'help': 'SH echo-able string that resolves to the path to the kernel spec directory',
+    })
+    launcher_exec: str = field(metadata={
+        'help': 'Executable used to launch the kernel (e.g., python)',
+    })
+    launcher_args: list[str] = field(metadata={
+        'help': 'Arguments to pass to the launcher executable',
+    })
+    launcher_version_env_var: str = field(metadata={
+        'help': 'Environment variable that contains the launcher version to check from compatible loaded modules',
+    })
+    kernel_version_env_var: str = field(metadata={
+        'help': 'Environment variable that contains the kernel version to be displayed',
+    })
+    kernel_resource_dir: str = field(default='', metadata={
+        'help': 'Path to the kernel resource directory relative to the kernel spec directory',
+    })
+
+    def __getitem__(self, key):
+        """Allow dictionary-like access to the attributes of the dataclass."""
+        return getattr(self, key)
+
+    def __iter__(self):
+        """Return an iterator over the attribute names of the dataclass."""
+        return iter(asdict(self))
+
+    def __len__(self):
+        """Return the number of attributes in the dataclass."""
+        return len(asdict(self))
+
+    def items(self):
+        """Return a dictionary of the attributes of the dataclass."""
+        return asdict(self).items()
+
+
+# Map a module name to the kernel spec information. This is used to determine how to extract the necessary information
+# from the environment module.
+MODULE_KERNEL_MAP: dict[str, ModuleKernelMapping] = {
+    'jupyter-server': ModuleKernelMapping(
+        kernel_display_name=f'{DISPLAY_PREFIX} Python',
+        kernel_language='python',
+        kernel_path='$EBROOTJUPYTERMINSERVER/share/jupyter/kernels/python3',
+        launcher_exec='python',
+        launcher_args=['-m', 'ipykernel_launcher', '-f', '{connection_file}'],
+        launcher_version_env_var='EBVERSIONPYTHON',
+        kernel_version_env_var='EBVERSIONPYTHON',
+    ),
+    'octave-kernel': ModuleKernelMapping(
+        kernel_display_name=f'{DISPLAY_PREFIX} Octave',
+        kernel_language='octave',
+        kernel_path='$EBROOTOCTAVEMINKERNEL/share/jupyter/kernels/octave',
+        launcher_exec='python',
+        launcher_args=['-m', 'octave_kernel', '-f', '{connection_file}'],
+        launcher_version_env_var='EBVERSIONPYTHON',
+        kernel_version_env_var='EBVERSIONOCTAVE',
+        kernel_resource_dir='images',
+    )
+}
 
 @dataclass
 class KernelData:
     """Data class to hold information about a kernel spec derived from an EasyBuild `jupyter-server` module.
     The metadata for each field includes an help and an optional getcmd key, which is a shell command to retrieve the
     value of the field."""
-    name: str = field(metadata={
+    mod_name: str = field(metadata={
         'help': 'Module name',
     })
-    version: str = field(metadata={
+    mod_version: str = field(metadata={
         'help': 'Module version',
     })
     kernel_path: str = field(metadata={
         'help': 'Path to the kernel spec directory',
-        'getcmd': 'echo $EBROOTJUPYTERMINSERVER/share/jupyter/kernels/python3'
+        'getcmd': 'echo {kernel_path}'
     })
-    py_exe: str = field(metadata={
+    kernel_version: str = field(metadata={
+        'help': 'Kernel version',
+        'getcmd': 'echo ${kernel_version_env_var}'
+    })
+    launcher_exe: str = field(metadata={
         'help': 'Path to the Python executable',
-        'getcmd': 'which python'
+        'getcmd': 'which {launcher_exec}'
     })
-    py_version: str = field(metadata={
+    launcher_version: str = field(metadata={
         'help': 'Python version',
-        'getcmd': 'echo $EBVERSIONPYTHON'
+        'getcmd': 'echo ${launcher_version_env_var}'
+    })
+    path: str = field(default='', metadata={
+        'help': 'PATH environment variable',
+        'getcmd': 'echo $PATH'
     })
     pythonpath: str = field(default='', metadata={
         'help': 'PYTHONPATH environment variable',
@@ -51,13 +131,17 @@ class KernelData:
         """Create a KernelData instance from an environment module."""
         mod_name, mod_ver = module.split('/')
 
+        info_map = MODULE_KERNEL_MAP.get(mod_name)
+
         cmds = [f"module load {module} > /dev/null 2>&1"]
         field_names = []
         for field_info in fields(cls):
+            field_name = field_info.name
             getcmd = field_info.metadata.get('getcmd')
             if getcmd:
+                getcmd = getcmd.format(**info_map)
                 cmds.append(getcmd)
-                field_names.append(field_info.name)
+                field_names.append(field_name)
 
         cmd = ' && '.join(cmds)
         try:
@@ -72,28 +156,34 @@ class KernelData:
         data_dct = {}
         for name, value in zip(field_names, output.decode('utf-8').splitlines()):
             data_dct[name] = value
-        return cls(name=mod_name, version=mod_ver, **data_dct)
+        return cls(mod_name=mod_name, mod_version=mod_ver, **data_dct)
 
 
 class EBKernelSpecManager(KernelSpecManager):
-    """Custom KernelSpecManager that finds kernels from EasyBuild's `jupyter-server` modules."""
+    """Custom KernelSpecManager that finds kernels from EasyBuild modules."""
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.found_specs: dict[str, KernelData] = {}
 
     def _find_jupyter_server_modules(self):
-        """Use module avail to find available Jupyter server modules."""
-        try:
-            output = subprocess.check_output(
-                'module --terse avail jupyter-server/',
-                shell=True, executable='/bin/bash',
-                stderr=subprocess.STDOUT
-            )
-            modules = output.decode('utf-8').splitlines()
-            return [line.strip() for line in modules if line.strip()]
-        except subprocess.CalledProcessError as e:
-            self.log.warning(f"Failed to find Jupyter  from environment modules: {e.output.decode('utf-8')}")
-            return []
+        """Use module avail to find available kenrnel-shipping modules."""
+        res = []
+
+        for module_name, _ in MODULE_KERNEL_MAP.items():
+            try:
+                output = subprocess.check_output(
+                    f'module --terse avail {module_name}/',
+                    shell=True, executable='/bin/bash',
+                    stderr=subprocess.STDOUT
+                )
+                modules = output.decode('utf-8').splitlines()
+                res += [line.strip() for line in modules if line.strip()]
+                # return [line.strip() for line in modules if line.strip()]
+            except subprocess.CalledProcessError as e:
+                self.log.warning(f"Failed to find {module_name} from environment modules: {e.output.decode('utf-8')}")
+                # return []
+
+        return res
 
     def find_kernel_specs(self):
         """Use the original method to find kernel specs, and then add any `jupyter-server` module kernels.
@@ -101,26 +191,29 @@ class EBKernelSpecManager(KernelSpecManager):
         """
         specs = super().find_kernel_specs()
 
-        current_eb_python = os.getenv('EBVERSIONPYTHON', None)
-
         server_modules = self._find_jupyter_server_modules()
         for mod in server_modules:
             if mod.startswith('/'):
                 continue
             data = KernelData.from_env_module(mod)
+
+            info_map = MODULE_KERNEL_MAP[data.mod_name]
+            current_launcher_version = os.getenv(info_map.launcher_version_env_var, None)
+            resource_dir  = os.path.join(data.kernel_path, info_map.kernel_resource_dir)
+
             # If another Easybuild Python is already loaded in the environment, with potentially other modules on top
             # of it (eg SciPy stack), avoid exposing kernels that would be incompatible with it
-            if current_eb_python and data.py_version != current_eb_python:
+            if current_launcher_version and data.launcher_version != current_launcher_version:
                 self.log.debug(
-                    f"Skipping kernel spec for {mod} (Python {data.py_version}) as it does not match current externally"
-                    f" loaded Python version {current_eb_python}"
+                    f"Skipping kernel spec for {mod} (Python {data.launcher_version}) as it does not match current "
+                    f"externally loaded Python version {current_launcher_version}"
                 )
                 continue
             # name = f"python{data.py_version}"
             name = mod.replace('/', '__')
-            specs[name] = data.kernel_path
+            specs[name] = resource_dir
             self.found_specs[name] = data
-            self.log.debug(f"Found kernel spec for {name}: {data.kernel_path} (Python {data.py_version})")
+            self.log.debug(f"Found kernel spec for {name}: {data.kernel_path} (Python {data.launcher_version})")
 
         return specs
 
@@ -141,6 +234,13 @@ class EBKernelSpecManager(KernelSpecManager):
         if not kernel_data:
             self.log.error(f"Kernel data for {kernel_name} not found in found_specs")
             raise ValueError(f"Kernel data for {kernel_name} not found in found_specs")
+
+        info_map = MODULE_KERNEL_MAP[kernel_data.mod_name]
+
+        launcher_args = info_map.launcher_args
+        display_name = info_map.kernel_display_name
+        language = info_map.kernel_language
+        resource_dir  = os.path.join(kernel_data.kernel_path, info_map.kernel_resource_dir)
 
         existing_ppath = os.getenv('PYTHONPATH', '').split(os.pathsep)
         # Ensure the pythonpath required for the kernel to work is added first
@@ -167,12 +267,12 @@ class EBKernelSpecManager(KernelSpecManager):
         prefixes = os.pathsep.join(filter(None, prefixes))
 
         kernel_dct = {
-            'argv': [
-                kernel_data.py_exe, '-m', 'ipykernel_launcher', '-f', '{connection_file}'
-            ],
-            'display_name': f"EasyBuilds' Python {kernel_data.py_version}",
-            'language': 'python',
+            'argv': [kernel_data.launcher_exe] + launcher_args,
+            'display_name': f"{display_name} ({kernel_data.kernel_version})",
+            'resource_dir': resource_dir,
+            'language': language,
             'env': {
+                'PATH': kernel_data.path,
                 'PYTHONPATH': ppath,
                 'EBPYTHONPREFIXES': prefixes,
                 # "EBPYTHONPREFIXES_DEBUG": "1",
